f80f1dd5c4c1064da54dda20ba4c51418e18b7c8
codernoyan <coder.noyan@gmail.com>
2023-03-13 23:17:25 +0600

    api created for crud

--- src/features/jobs/jobsAPI.js
+++ src/features/jobs/jobsAPI.js
@@ -5,3 +5,17 @@ export const getJobs = async () => {
   return response.data;
 };
 
+export const addJob = async (jobData) => {
+  const response = await axios.post('/jobs', jobData);
+  return response;
+};
+
+export const updateJob = async (id, jobData) => {
+  const response = await axios.patch(`/jobs/${id}`, jobData);
+  return response;
+};
+
+export const deleteJob = async (id) => {
+  const response = await axios.delete(`/jobs/${id}`);
+  return response;
+};

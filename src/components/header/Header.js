35aadff9d314bd6ef883572de89580be928ddf2a
codernoyan <coder.noyan@gmail.com>
2023-03-13 13:52:26 +0600

    bugs fixed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,8 +2,8 @@ export default function Header() {
   return (
     <div className="md:flex space-y-2 md:space-y-0 justify-between mb-10 ">
       <h1 className="lws-section-title">All Available Jobs</h1>
-      <div className="flex gap-4">
-        <form>
+      <form>
+        <div className="flex gap-4">
           <div className="search-field group flex-1">
             <i className="fa-solid fa-magnifying-glass search-icon group-focus-within:text-blue-500" />
             <input type="text" placeholder="Search Job" className="search-input" id="lws-searchJob" />
@@ -13,8 +13,8 @@ export default function Header() {
             <option value="low_to_high">Salary (Low to High)</option>
             <option value="high_to_low">Salary (High to Low)</option>
           </select>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   )
 };

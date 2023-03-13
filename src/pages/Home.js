386349320652630d5042358c60f3b2cd12d58319
codernoyan <coder.noyan@gmail.com>
2023-03-13 13:31:53 +0600

    header modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import Header from "../components/header/Header";
 import Sidebar from "../components/sidebar/Sidebar";
 
 export default function Home() {
@@ -6,20 +7,7 @@ export default function Home() {
       <Sidebar />
       <div className="lg:pl-[14rem]  mt-[5.8125rem]">
         <main className="max-w-3xl rounded-lg  mx-auto relative z-20 p-10 xl:max-w-none bg-[#1E293B]">
-          <div className="md:flex space-y-2 md:space-y-0 justify-between mb-10 ">
-            <h1 className="lws-section-title">All Available Jobs</h1>
-            <div className="flex gap-4">
-              <div className="search-field group flex-1">
-                <i className="fa-solid fa-magnifying-glass search-icon group-focus-within:text-blue-500" />
-                <input type="text" placeholder="Search Job" className="search-input" id="lws-searchJob" />
-              </div>
-              <select id="lws-sort" name="sort" autoComplete="sort" className="flex-1">
-                <option>Default</option>
-                <option>Salary (Low to High)</option>
-                <option>Salary (High to Low)</option>
-              </select>
-            </div>
-          </div>
+          <Header />
           <div className="jobs-list">
             {/* Single Job 1*/}
             <div className="lws-single-job">
